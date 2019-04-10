2d7ccf7376f810554857c4e66401e90f61851e0b
RuleofThree <derekwhite662@gmail.com>
2019-04-10 17:31:04 -0500

    none

--- charFeats.py
+++ charFeats.py
@@ -15,6 +15,18 @@ def dexfeatList():
     dexterity = featInfo['dexterity']
     for keys in dexterity:
         print(keys)
+    select = input("For information on a listed feat, type 'help <feat>'\n"
+          "To choose a listed feet, type 'choose <feat>': ")
+    if select[:4] == 'help':
+        featSelected = select[5:]
+        desc = dexterity[featSelected]['desc']
+        level = dexterity[featSelected]['requirements'][0]
+        reqStr = dexterity[featSelected]['requirements'][1]
+        reqDex = dexterity[featSelected]['requirements'][2]
+        reqCon = dexterity[featSelected]['requirements'][3]
+        reqFeats = dexterity[featSelected]['requirements'][4]
+        print(desc)
+        print("Prerequisites: " + "Level: " + str(level) + " Strength: " + str(reqStr) + " Dexterity: " + str(reqDex) + " Constitution: " + str(reqCon) + " Required Feats: " + reqFeats)
 
 def confeatList():
     featFile = open("feats.txt", "r", encoding="utf-8")
--- diceGame.py
+++ diceGame.py
@@ -3,26 +3,47 @@ import charCreation
 import charInfo
 import charFeats
 
+class Hub(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = "Hub > "
+        print("Welcome to the Hub. From here, you can create your first character and view the character sheet with the"
+              " 'char' command, or review and select feats with the 'feat' command. For more information on both, please"
+              " type help <command>.")
+
+    def  do_char(self, args):
+        """this command will give you access to the 'start' command, to create your character for this account. It
+will also give you access to the 'viewchar' command, which will provide you with a basic character sheet
+containing all needed information"""
+        character = Character()
+        character.cmdloop()
+
+    def do_feat(self, args):
+        """this command will give you access to the commands to look at all feats in the game and their descriptions
+and any required qualifications that are needed before taking the feat. Feats are divided into four catagories:
+Strength, Dexterity, Constition, and Magic.
+        """
+        feats = Feats()
+        feats.cmdloop()
+
+    def do_quit(self, args):
+        """Exits the bot"""
+        print("Closing closing the bot.")
+        return True
+
 class Character(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
+        self.prompt = "Character > "
         print("Welcome to Character Creation. To begin, type 'start'. To exit, type 'quit'. For a list of commands and"
               " their descriptions, type 'help'")
 
     def do_start(self, args):
-        """Use this command to create a new character. Note you can only have one character per account. Feats are selected using the 'feats' option"""
+        """Use this command to create a new character. Note you can only have one character per account. Feats are
+        selected using the 'feats' option"""
         charStats = charCreation.basics()
         charMods = charCreation.abilities(charStats)
         charCreation.save(charStats, charMods)
-
-    def do_liststrfeats(self, args):
-        charFeats.strfeatList()
-
-    def do_listdexfeats(self, args):
-        charFeats.dexfeatList()
-
-    def do_listconfeats(self, args):
-        charFeats.confeatList()
 
     def do_viewchar(self, args):
         """Use this command to get a list of your character statics"""
@@ -33,6 +54,28 @@ class Character(cmd.Cmd):
         print("Closing Character Creation.")
         return True
 
+class Feats(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = "Feats > "
+        print("Welcome to Feat Selection. Here you can view the lists of feats for any given category: Strength"
+              " Dexterity, Constitution, and Magic. Simply type 'listfeats' and then the first three letters of the"
+              " category you wish to see. Example: To see strength feats, type 'listfeatsstr'. For further information"
+              " type 'help' for a list of commands")
+
+    def do_listfeatsstr(self, args):
+        charFeats.strfeatList()
+
+    def do_listfeatsdex(self, args):
+        charFeats.dexfeatList()
+
+    def do_listfeatscon(self, args):
+        charFeats.confeatList()
+
+    def do_quit(self, args):
+        """Leaves Feat Selection"""
+        print("Closing Feat Selection.")
+        return True
 if __name__ == "__main__":
-    character = Character()
-    character.cmdloop()
+    hub = Hub()
+    hub.cmdloop()
